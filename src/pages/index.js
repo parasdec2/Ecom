5db48c98cd479558dfd24aadb85833f223afb529
Paras Garg <paras.dec2@gmail.com>
2019-06-21 17:30:25 +0530

    Deleting files and bring in assets

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
@@ -11,10 +10,10 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}> 
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/contact/">Contact Us</Link>
+    <Link to="/about/">About Us</Link>
   </Layout>
 )
 
